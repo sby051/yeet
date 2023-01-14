b8ae3756a5e4797b00f90e7f51c5137fb8bc06db
sby051 <91495085+sby051@users.noreply.github.com>
2023-01-14 03:04:40 +0000

    wefwef

--- install.sh
+++ install.sh
@@ -12,17 +12,8 @@ sudo chown $USER:$USER /opt/yeet
 # Copy files
 cp -r ./bin /opt/yeet/bin
 
-# detect which shell is being used
-if [ -n "$ZSH_VERSION" ]; then
-    echo "export PATH=/opt/yeet/bin:$PATH" >> ~/.zshrc
-elif [ -n "$BASH_VERSION" ]; then
-    echo "export PATH=/opt/yeet/bin:$PATH" >> ~/.bashrc
-elif [ -n "$FISH_VERSION" ]; then
-    echo "\n#yeet start\nset PATH /opt/yeet/bin $PATH\n#yeet" >> ~/.config/fish/config.fish
-fi
-
-# Reload shell
-exec $SHELL
-
+echo "export PATH=/opt/yeet/bin:\$PATH" >> ~/.zshrc
+echo "export PATH=/opt/yeet/bin:\$PATH" >> ~/.bashrc
+echo "set PATH /opt/yeet/bin \$PATH" >> ~/.config/fish/config.fish
 # Done
-echo "Yeet has been installed successfully!"
+echo "Yeet has been installed successfully! Please restart your terminal."
