2438451fb2af94eeb05ea8b6d39143c0301f4833
sby051 <91495085+sby051@users.noreply.github.com>
2023-01-14 02:50:33 +0000

    big commits

--- yeet.py
+++ yeet.py
@@ -3,6 +3,7 @@ from argparse import ArgumentParser
 from shutil import move
 from os import getcwd, mkdir, path, chdir, system
 from time import time
+import datetime
 import json
 import shutil
 
@@ -11,10 +12,49 @@ CWD = getcwd()
 USER_DIR = path.expanduser("~")
 YEET_DIR = f"{USER_DIR}/.yeet"
 YEETED_JSON = f"{YEET_DIR}/yeeted.json"
-YEET_EXPIRY_SECONDS = 60 * 60 * 24 * 7 # 7 days 
+YEET_EXPIRY_SECONDS = 60 * 60 * 24 * 7 # 7 days
 CURRENT_TIME = time()
+PARSER_OPTIONS = [
+    {
+        "short": "r",
+        "long": "_restore",
+        "help": "Restore a file from the yeet bin",
+        "action": "store_true"
+    },
+    {
+        "short": "l",
+        "long": "list",
+        "help": "List all yeeted files",
+        "action": "store_true"
+    },
+    {
+        "short": "e",
+        "long": "_empty",
+        "help": "Empty the _yeet bin",
+        "action": "store_true"
+    },
+    {
+        "short": "y",
+        "long": "yes",
+        "help": "Automatically confirm all prompts",
+        "action": "store_true"
+    },
+    {
+        "short": "v",
+        "long": "version",
+        "help": "Print the version and exit",
+        "action": "version"
+    },
+]
+PARSER_DETAILS = {
+    "description": "Yeet files to the _yeet bin and restore them later.",
+    "epilog": "GitHub: https://github.com/sby051/yeet",
+    "version": "1.0.0",
+    "usage": "yeet {options} [file]",
+    "prog": "yeet",
+}
 
-def init_yeet() -> None: # Creates the yeet directory and yeet.json file if they don't exist
+def _init_yeet() -> None:
     if not path.exists(YEET_DIR):
         mkdir(YEET_DIR)
     
@@ -22,8 +62,25 @@ def init_yeet() -> None: # Creates the yeet directory and yeet.json file if they
         with open(YEETED_JSON, "w") as f:
             f.write("{}")
             
-# Moves a file to the yeet directory and logs its name as the key and its original location as the value
-def yeet(file: str) -> None:
+def _get_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+    
+    for detail in PARSER_DETAILS:
+        parser.__setattr__(detail, PARSER_DETAILS[detail])
+    
+    parser.add_argument("file", help="The file to _yeet or _restore", nargs="?")
+    
+    for option in PARSER_OPTIONS:
+        parser.add_argument(
+            f"-{option['short']}",
+            f"--{option['long']}",
+            help=option["help"],
+            action=option["action"]
+        )
+
+    return parser
+            
+def _yeet(file: str) -> None:
     absolute_path = f"{CWD}/{file}"
     
     if not path.exists(absolute_path):
@@ -34,10 +91,10 @@ def yeet(file: str) -> None:
         print(f"File {file} has already been yeeted.")
         return
     
-    # Move the file to the yeet directory
+    # Move the file to the _yeet directory
     move(absolute_path, YEET_DIR)
     
-    # Add the file to the yeet.json file
+    # Add the file to the _yeet.json file
     with open(YEETED_JSON, "r") as f:
         yeeted_json = json.load(f)
         
@@ -51,8 +108,7 @@ def yeet(file: str) -> None:
     
     print(f"Yeeted {file} to {YEET_DIR}")
     
-# Restores a file from the yeet directory to its original location
-def restore(file: str) -> None:
+def _restore(file: str) -> None:
     with open(YEETED_JSON, "r") as f:
         yeet_json = json.load(f)
         
@@ -74,19 +130,28 @@ def restore(file: str) -> None:
         json.dump(yeet_json, f)
     
     print(f"Restored {file} to {original_location}")
-
-def list_yeeted() -> None:
-    print("Yeeted files:")
     
-    with open(f"{YEET_DIR}/yeet.json", "r") as f:
+def _time_to_date(timestamp: float) -> str:
+    x = datetime.datetime(1970, 1, 1) + datetime.timedelta(seconds=timestamp)
+    return x.strftime("%d/%m/%Y at %H:%M:%S")
+
+def _list_yeeted() -> None:
+    
+    with open(f"{YEETED_JSON}", "r") as f:
         yeet_json = json.load(f)
         
-    for file in yeet_json:
-        print(f"{file} -> {yeet_json[file]}")
-    else:
-        print("No yeeted files.")
+    if not yeet_json:
+        print("No yeeted files (yet). Try yeeting some files!")
+        return
     
-def confirm(prompt: str, callback: callable) -> None:
+    print("Yeeted files:")
+    for file in yeet_json:
+        print(f"""{file}:
+  Expiry: {_time_to_date(yeet_json[file]['expires'])}
+  Original Path: {yeet_json[file]['path']}
+    """)
+    
+def _confirm(prompt: str, callback: callable) -> None:
     print(prompt)
     response = input("y/n: ")
     
@@ -96,41 +161,56 @@ def confirm(prompt: str, callback: callable) -> None:
         print("Aborted.")
         return
     
-def empty() -> None:
+def _empty() -> None:
     shutil.rmtree(YEET_DIR)
-    init_yeet()
+    _init_yeet()
     print("Yeet bin emptied.")
+    
+def _check_expiry() -> None:
+    with open(YEETED_JSON, "r") as f:
+        yeet_json = json.load(f)
+        
+    new_yeet_json = {}
+        
+    for file in yeet_json:
+        if CURRENT_TIME >= yeet_json[file]["expires"]:
+            system(f"rm -rf {YEET_DIR}/{file}")
+            print(f"[!] File '{file}' has expired and has been deleted.")
+        else:
+            new_yeet_json[file] = yeet_json[file]
+            
+            
+    with open(YEETED_JSON, "w") as f:
+        json.dump(new_yeet_json, f)
 
 def main():
-    parser = ArgumentParser()
-    parser.description = "A rubbish bin for Linux"
-    parser.epilog = "GitHub: https://github.com/sby051/yeet"
-    parser.version = "0.1"
-    parser.prog = "yeet"
-    parser.usage = "yeet [options] {restore | list | empty} [file]"
-    
-    init_yeet()    
-
-    parser.add_argument("action", help="The action to perform", action="store", nargs="?", default="yeet", choices=["yeet", "restore", "list", "empty"])
-    parser.add_argument("file", help="The file to yeet or restore", nargs="?")
-    parser.add_argument("-f", "--force", help="Force the action", action="store_true")
-    parser.add_argument("-v", "--version", help="Print the version and exit", action="version")   
+    parser = _get_parser()
+    _init_yeet()
+    _check_expiry()
     
     args = parser.parse_args()
     
-    # if no action is specified, yeet the file
-    if args.action == "restore":
-        restore(args.file)
-    elif args.action == "list":
-        list_yeeted()
-    elif args.action == "empty":
-        if args.force:
-            empty()
-        else:
-            confirm("Are you sure you want to empty the yeet bin? This cannot be undone.", empty)
-    else:
-        yeet(args.file)
+    if not args.file and not args._empty and not args.list:
+        parser.print_usage()
+        return
     
+    if args._restore:
+        if args.yes:
+            _restore(args.file)
+        else:
+            _confirm(f"Are you sure you want to restore {args.file}?", lambda: _restore(args.file))
+        
+    if args.list:
+        _list_yeeted()
+        
+    if args._empty:
+        if args.yes:
+            _empty()
+        else:
+            _confirm("Are you sure you want to empty the yeet bin?", _empty)
+
+    if not args._restore and not args.list and not args._empty:
+        _yeet(args.file)
     
 if __name__ == '__main__':
     main()
