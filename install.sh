a3f83200ccec0e11c833bdd4fe98045c36a4bceb
sby051 <91495085+sby051@users.noreply.github.com>
2023-01-14 11:51:10 +0000

    update

--- install.sh
+++ install.sh
@@ -5,20 +5,18 @@ if [ -d "/opt/yeet" ]; then
     exit 1
 fi
 
-sudo mkdir /usr/bin/yeet
-sudo touch /usr/bin/yeet/yeet
-curl -L "https://github.com/sby051/yeet/blob/main/bin/yeet?raw=true" > /usr/bin/yeet/yeet
+curl -L "https://github.com/sby051/yeet/blob/main/bin/yeet?raw=true" > /usr/bin/yeet
 
-# determine shell
-if [ -n "$ZSH_VERSION" ]; then
-    echo "export PATH=\$PATH:/opt/yeet/bin" >> ~/.zshrc
-elif [ -n "$BASH_VERSION" ]; then
-    echo "export PATH=\$PATH:/opt/yeet/bin" >> ~/.bashrc
-elif [ -n "$FISH_VERSION" ]; then
-    echo "set -x PATH \$PATH /opt/yeet/bin" >> ~/.config/fish/config.fish
-else
-    echo "Yeet only supports bash, fish and zsh"
-    exit 1
+# split the shell path to get the shell name
+shell_name=${SHELL[-1]}
+
+# add yeet to path in the shell config file
+if [ "$shell_name" = "bash" ]; then
+    echo "export PATH=\$PATH:/usr/bin" >> ~/.bashrc
+elif [ "$shell_name" = "zsh" ]; then
+    echo "export PATH=\$PATH:/usr/bin" >> ~/.zshrc
+elif [ "$shell_name" = "fish" ]; then
+    echo "set -x PATH \$PATH /usr/bin" >> ~/.config/fish/config.fish
 fi
 
 # Done
