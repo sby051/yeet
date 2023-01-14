3c322ac3a1de65c0e72a932f74631d7bbe78ba83
sby051 <91495085+sby051@users.noreply.github.com>
2023-01-14 12:03:01 +0000

    update

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ pip install pyinstaller
 
 pyinstaller --onefile ./src/yeet.py
 
+# create bin folder if it doesn't exist
+mkdir -p ./bin
 mv ./dist/yeet ./bin/yeet
 rm -rf ./dist
 rm -rf ./yeet.spec
